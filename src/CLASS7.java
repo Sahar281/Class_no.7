5ecc8532b109a4e2c2062a4686162ce13ec69762
sahar281 <saharpilo13@gmail.com>
2021-06-21 12:56:06 +0300

    version5.1

---
+++ src/CLASS7.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class CLASS7 {
+
+        @Test
+        public void test_01(){
+            System.out.println(1);
+            System.out.println("3");
+            System.out.println("5.1");
+        }
+}
